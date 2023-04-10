845af2a2d7ac56ae4cc63e33aaed0f8fe578273a
Dima Chachin <chachin99@bk.ru>
2023-04-10 19:45:20 +0300

    Homework

---
+++ Exemple_1.java
@@ -0,0 +1,11 @@
+package Homework_6;
+
+import Homework_6.NoteBook.main_memory;
+
+public class Exemple_1 {
+    public static void main(String[] args) {
+        NoteBook nb = new NoteBook();
+
+    }
+
+}
---
+++ NoteBook.java
@@ -0,0 +1,28 @@
+package Homework_6;
+
+public class NoteBook {
+
+    public class main_memory {
+        
+        int volume;
+        String type;
+    }
+
+    public class static_memory {
+    
+        int volume;
+        String type;
+    }
+
+    public class operating_system {
+    
+        String type;
+        String version;
+    }
+
+    public class screen {
+    
+        double size;
+        String type;
+    }
+}
